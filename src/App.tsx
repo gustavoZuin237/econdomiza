f682c672bc4be75b335dab967b271555d6799efe
gustavoZuin237 <zuingustavo206@gmail.com>
2025-05-22 15:37:56 -0300

    Feat: added theme file and ThemeProvider component to App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { ThemeProvider } from "@mui/material"
+import { theme } from "./styles/theme"
+
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       App
-    </>
+    </ThemeProvider>
   )
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#007742',
+      
+    },
+    secondary: {
+      main: '#fea326',
+    },
+  },
+});
